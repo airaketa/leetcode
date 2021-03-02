5dd798aba8bdee6149d5a925eadcdf76d182d1e2
airaketa <jk3000@yandex.ru>
2021-03-02 17:26:16 +0300

    LC-645

--- com.airaketa.leetcode/src/com/airaketa/leetcode/lc645/Solution.java
+++ com.airaketa.leetcode/src/com/airaketa/leetcode/lc645/Solution.java
@@ -1,4 +1,28 @@
 package com.airaketa.leetcode.lc645;
 
-public class Solution {
+/**
+ * https://leetcode.com/problems/set-mismatch/
+ */
+class Solution {
+    public static int[] findErrorNums(int[] nums) {
+        int[] count = new int[nums.length];
+        int doubled = 0;
+        int absent = 0;
+        for (int i = 0; i < nums.length; i++)
+        {
+            if (++count[nums[i] - 1] == 2)
+            {
+                doubled = nums[i];
+            }
+        }
+        for (int i = 0; i < count.length; i++)
+        {
+            if (count[i] == 0)
+            {
+                absent = i + 1;
+                break;
+            }
+        }
+        return new int[]{doubled, absent};
+    }
 }

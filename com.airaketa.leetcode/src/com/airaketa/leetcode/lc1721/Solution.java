8c57c1b18c7d989cd8e076b337275398d0267c80
airaketa <jk3000@yandex.ru>
2021-03-14 19:22:26 +0300

    LC-1721 v2

--- com.airaketa.leetcode/src/com/airaketa/leetcode/lc1721/Solution.java
+++ com.airaketa.leetcode/src/com/airaketa/leetcode/lc1721/Solution.java
@@ -1,42 +1,57 @@
 package com.airaketa.leetcode.lc1721;
 
-import java.util.List;
-import java.util.ArrayList;
-
 /**
  * https://leetcode.com/problems/swapping-nodes-in-a-linked-list/
  */
 class Solution {
-    public ListNode swapNodes(ListNode head, int k) {
-        List<Integer> nodes = new ArrayList<>();
 
+    public ListNode swapNodes(ListNode head, int k) {
+        int i = 1;
+        ListNode first = head;
+        ListNode last = head;
         ListNode current = head;
         while (current != null) {
-            nodes.add(current.val);
+            if (i == k) {
+                first = current;
+            }
+            else if (i > k) {
+                last = last.next;
+            }
+            i++;
             current = current.next;
         }
-
-        int firstIndex = k - 1;
-        int lastIndex = nodes.size() - k;
-        int first = nodes.get(firstIndex);
-        int last = nodes.get(lastIndex);
-
-        nodes.set(lastIndex, first);
-        nodes.set(firstIndex, last);
-
-        ListNode newHead = new ListNode(nodes.get(0));
-        current = newHead;
-        for (int i = 1; i < nodes.size(); i++) {
-            ListNode next = new ListNode(nodes.get(i));
-            current.next = next;
-            current = next;
-        }
-        return newHead;
+        int buf = first.val;
+        first.val = last.val;
+        last.val = buf;
+        return head;
     }
 
-    public static void main(String[] args) {
-        new Solution().swapNodes(new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5))))), 2);
-    }
+//        public ListNode swapNodes(ListNode head, int k) {
+//        List<Integer> nodes = new ArrayList<>();
+//
+//        ListNode current = head;
+//        while (current != null) {
+//            nodes.add(current.val);
+//            current = current.next;
+//        }
+//
+//        int firstIndex = k - 1;
+//        int lastIndex = nodes.size() - k;
+//        int first = nodes.get(firstIndex);
+//        int last = nodes.get(lastIndex);
+//
+//        nodes.set(lastIndex, first);
+//        nodes.set(firstIndex, last);
+//
+//        ListNode newHead = new ListNode(nodes.get(0));
+//        current = newHead;
+//        for (int i = 1; i < nodes.size(); i++) {
+//            ListNode next = new ListNode(nodes.get(i));
+//            current.next = next;
+//            current = next;
+//        }
+//        return newHead;
+//    }
 
     public static class ListNode {
         int val;
